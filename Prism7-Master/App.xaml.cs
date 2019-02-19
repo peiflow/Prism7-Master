1f13cf6fc851b79a19f9f289356f7730827aa6f2
Pablo Alvarez <palvarezg.work@outlook.es>
2019-02-19 20:15:22 +0100

    Content module initialization mode

--- Prism7-Master/App.xaml.cs
+++ Prism7-Master/App.xaml.cs
@@ -24,7 +24,7 @@ namespace Prism7_Master
         protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
         {
             moduleCatalog.AddModule<ModuleMenu.MenuModule>(InitializationMode.WhenAvailable);
-            moduleCatalog.AddModule<ModuleContent.ContentModule>(InitializationMode.OnDemand);
+            moduleCatalog.AddModule<ModuleContent.ContentModule>(InitializationMode.WhenAvailable);
 
         }
     }
